b4ab5884450bb34ebba3ef2fc11162f8f5159f16
Hanna Kim <hannakim91@gmail.com>
2021-02-16 16:42:04 -0700

    Create function to return random greeting

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -14,9 +16,26 @@ func Hello(name string) (string, error) {
 	// if name was received, return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+	// nil = no error
 }
 
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+			"Hi, %v. Welcome!",
+			"Great to see you, %v!",
+			"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
 // := is shorthand operator - declare + Initialize variable in one line
 // alternative:
 // 	var message string
 // 	message = fmt.Sprintf("Hi %v. Welcome!", name)
+
+// go function can return multiple values
